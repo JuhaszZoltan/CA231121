c3ab0009b2f4ca0167517703ede32d6cf624fc11
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2023-11-21 18:56:31 +0100

    Add project files.

---
+++ JatekosLovese.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA231121
+{
+    internal class JatekosLovese
+    {
+        public static (double X, double Y) Cel = (0f, 0f);
+
+        public int LovesSorszam { get; set; }
+        public string Nev { get; set; }
+        public (double X, double Y) Loves { get; set; }
+        public double Tavolsag
+        {
+            get
+            {
+                double dx = Cel.X - Loves.X;
+                double dy = Cel.Y - Loves.Y;
+                return Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dy, 2));
+            }
+        }
+
+        public double Pontszam
+        {
+            get
+            {
+                double psz = Math.Round(10 - Tavolsag, 2);
+                return psz > 0 ? psz : 0d;
+            }
+        }
+
+        public JatekosLovese(string sor, int sorszam)
+        {
+            LovesSorszam = sorszam;
+            var v = sor.Split(';');
+            Nev = v[0];
+            Loves = (double.Parse(v[1]), double.Parse(v[2]));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using CA231121;
+using System.Text;
+
+
+List<JatekosLovese> lovesek = new();
+using StreamReader sr = new(@"..\..\..\src\lovesek.txt", Encoding.UTF8);
+var cel = sr.ReadLine().Split(';');
+JatekosLovese.Cel = (double.Parse(cel[0]), double.Parse(cel[1]));
+int sorszam = 1;
+while(!sr.EndOfStream)
+{
+    lovesek.Add(new(sr.ReadLine(), sorszam));
+    sorszam++;
+}
+
+Console.WriteLine($"5. feladat: Lövések száma: {lovesek.Count} db");
+
+Console.WriteLine("7. feladat: Legpontosabb lövés:");
+var f7 = lovesek.MinBy(l => l.Tavolsag);
+Console.WriteLine(
+    $"\t{f7.LovesSorszam}.\n" +
+    $"\t{f7.Nev}\n" +
+    $"\tx={f7.Loves.X}\n" +
+    $"\ty={f7.Loves.Y}\n" +
+    $"\ttávolság: {f7.Tavolsag}");
+
+var f9 = lovesek.Count(l => l.Pontszam == 0d);
+Console.WriteLine($"9. feladat: Nulla pontos lövések száma: {f9} db");
+
+var f10 = lovesek.Select(l => l.Nev).Distinct().Count();
+Console.WriteLine($"10. feladat: játékosok száma: {f10}");
+
+Console.WriteLine($"Lövések száma (játékosonként):");
+var dic = lovesek
+    .GroupBy(l => l.Nev)
+    .ToDictionary(d => d.Key, d => d.ToList());
+foreach (var kvp in dic)
+    Console.WriteLine($"\t{kvp.Key} - {kvp.Value.Count} db");
+
+Console.WriteLine("Átlagpontszámok (játékosonként):");
+foreach (var kvp in dic)
+    Console.WriteLine($"{kvp.Key} - {kvp.Value.Average(l => l.Pontszam)}");
+
+string f13 = dic.MaxBy(d => d.Value.Average(l => l.Pontszam)).Key;
+Console.WriteLine($"13. feladat: A játék nyertese: {f13}");
